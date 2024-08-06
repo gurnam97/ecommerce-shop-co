1cd29384ef66c3b519df2392607eb850f95c62a0
gurnam97 <gurnam.singh@chicos.com>
2024-08-07 00:54:48 +0530

    fix: code cleanup

---
+++ src/components/Header/__fixtures__/menuItems.ts
@@ -0,0 +1,6 @@
+export const menuItemsList = [
+  { name: "Shop", url: "/shop" },
+  { name: "On Sale", url: "/sale" },
+  { name: "New Arrivals", url: "/new-arrivals" },
+  { name: "Brands", url: "/brands" },
+];
